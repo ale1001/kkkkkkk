d686637db1636469c7307d2da4b8c55e0d4c5d26
ale1001 <64748782+ale1001@users.noreply.github.com>
2020-05-04 19:56:47 -0300

    Add files via upload

---
+++ cc.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace cc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, resultado;
+            Console.WriteLine("Informe o primeiro número:");
+            n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o segundo número;");
+            n2 = int.Parse(Console.ReadLine());
+
+            resultado = n1 - n2;
+
+            Console.WriteLine("O resultado é: " + resultado);
+            Console.ReadKey(); 
+
+        }
+    }
+}
